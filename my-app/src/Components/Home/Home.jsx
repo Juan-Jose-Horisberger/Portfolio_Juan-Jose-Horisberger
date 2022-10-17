1790c34866eebc945b96c0f6c985cf8c41f84d80
Juan Jose Horisberger <juanjhorisberger@gmail.com>
2022-10-17 16:34:45 -0300

    Actualizacion de CV

--- my-app/src/Components/Home/Home.jsx
+++ my-app/src/Components/Home/Home.jsx
@@ -11,7 +11,7 @@ import { AiOutlineGithub } from "react-icons/ai";
 import emailjs from "@emailjs/browser";
 import * as Scroll from 'react-scroll';
 import { Element } from 'react-scroll'
-import documentCv from "../../Curriculum de Juan Jose Horisberger 2022.pdf"
+import documentCv from "../../CV de Juan Jose Horisberger.pdf"
 
 /*Iconos*/
 import imgHtml from "../../imagenes/imgHtml.svg";
@@ -201,6 +201,9 @@ export default function Home() {
             </Element>
 
 
+            <div class='container_img'>
+                <img src="aca hay una imagen xd" alt="image" />
+            </div>
 
             <div className={`${styles.container_Proyects}`} style={{ marginTop: "130px" }}>
                 <div className={`${styles.container_Section1}`}>
